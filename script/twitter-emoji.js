e4e31612167178db79b3c1cda5061ee469b64700
Azraias <32257015+Carlos920@users.noreply.github.com>
2023-01-05 08:43:28 +0800

    update

--- script/twitter-emoji.js
+++ script/twitter-emoji.js
@@ -11,25 +11,14 @@
 // @require      https://twemoji.maxcdn.com/v/latest/twemoji.min.js
 // ==/UserScript==
 
+(e=>{const o=document.createElement("style");o.innerText=e,document.head.appendChild(o)})(`img.twitter-emoji {height: 1em;width: 1em;margin: 0 .05em 0 .1em !important;vertical-align: -0.1em;display:inline-block;}`);
+		/*vertical-align: -0.1em;*/
 (function() {
     'use strict';
     // 文档：https://github.com/twitter/twemoji
-
-    var head = document.head || document.getElementsByTagName("head")[0];
-
-    var style = document.createElement("style");
-    style.innerHTML = `/* twemoji styles */
-	img.twitter-emoji {
-		height: 1em;
-		width: 1em;
-		margin: 0 .05em 0 .1em;
-		/*vertical-align: -0.1em;*/
-	}`;
-    head.appendChild(style);
-
     twemoji.parse(document.body, {
-        className: "twitter-emoji",
-        folder: "svg",
-        ext: ".svg",
-    });
+                className: "twitter-emoji",
+                folder: "svg",
+                ext: ".svg",
+            });
 })();
